fee11e45848aa132d23ea94dc2343ebb80064f47
Rik Smith-Unna <rik@fathomlabs.io>
2022-03-06 21:24:23 +0000

    integrate openlab-applayer-client, implement app list (closes #13)

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import Conf from 'conf'
 const userConfig = new Conf({
   defaults: {
     openlab: {
-      baseUrl: 'http://toolchest.apps.openlab-api.fibrill.ae',
+      baseUrl: 'http://discovery.service.openlab.run',
     },
     estuary: {
       clientKeyUrl: 'https://estuary-auth.labdao-application-layer.workers.dev',
