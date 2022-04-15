452228baa63c0e12b58f147cd5319d3816a39ed3
Rik Smith-Unna <rik@fathomlabs.io>
2022-04-15 03:23:14 +0100

    Remove unused imports and variables, standardise formatting

--- src/commands/exchange/closedjobs.ts
+++ src/commands/exchange/closedjobs.ts
@@ -1,43 +1,43 @@
-import {CliUx, Command, Flags} from '@oclif/core'
+import { Command } from '@oclif/core'
 const axios = require('axios')
 
 export default class ExchangeClosedJobs extends Command {
 
-    // Update when new subgraph is deployed
-    static APIURL = `https://api.thegraph.com/subgraphs/name/tohrnii/openlab-exchange-mumbai-c`
+  // Update when new subgraph is deployed
+  static APIURL = `https://api.thegraph.com/subgraphs/name/tohrnii/openlab-exchange-mumbai-c`
 
-    static query = `
-        query {
-            jobs(where: {status: 2}) {
-                id
-                jobURI
-                openlabNFTURI
-            }
-        }
-    `
+  static query = `
+  query {
+      jobs(where: {status: 2}) {
+          id
+          jobURI
+          openlabNFTURI
+      }
+  }
+  `
 
-    public async run(): Promise<void> {
-        const {
-            args,
-            flags,
-        } = await this.parse(ExchangeClosedJobs)
-        const result = await axios.post(
-            ExchangeClosedJobs.APIURL, 
-            {
-                query: ExchangeClosedJobs.query
-            }
-        )
-        const jobs = result.data.data.jobs
-        if (jobs.length == 0) {
-            this.log('NO CLOSED JOBS')
-        } else {
-            this.log('CLOSED JOBS...')
-            for (let i=0; i<jobs.length; i++) {
-                // Substring to remove '0x' prefix on job id
-                this.log(`Job ID: ${jobs[i].id.substring(2)}`)
-                this.log(`Job Metadata IPFS CID: ${jobs[i].jobURI.substring(5)}`)
-                this.log(`Openlab NFT IPFS CID: ${jobs[i].openlabNFTURI.substring(5)}`)
-            }
-        }
+  public async run(): Promise<void> {
+    const {
+      args,
+      flags,
+    } = await this.parse(ExchangeClosedJobs)
+    const result = await axios.post(
+      ExchangeClosedJobs.APIURL,
+      {
+        query: ExchangeClosedJobs.query
+      }
+    )
+    const jobs = result.data.data.jobs
+    if (jobs.length == 0) {
+      this.log('NO CLOSED JOBS')
+    } else {
+      this.log('CLOSED JOBS...')
+      for (let i = 0; i < jobs.length; i++) {
+        // Substring to remove '0x' prefix on job id
+        this.log(`Job ID: ${jobs[i].id.substring(2)}`)
+        this.log(`Job Metadata IPFS CID: ${jobs[i].jobURI.substring(5)}`)
+        this.log(`Openlab NFT IPFS CID: ${jobs[i].openlabNFTURI.substring(5)}`)
+      }
     }
+  }
 }
--- src/commands/exchange/openjobs.ts
+++ src/commands/exchange/openjobs.ts
@@ -1,42 +1,38 @@
-import {CliUx, Command, Flags} from '@oclif/core'
+import { Command } from '@oclif/core'
 const axios = require('axios')
 
 export default class ExchangeOpenJobs extends Command {
 
-    // Update when new subgraph is deployed
-    static APIURL = `https://api.thegraph.com/subgraphs/name/tohrnii/openlab-exchange-mumbai-c`
+  // Update when new subgraph is deployed
+  static APIURL = `https://api.thegraph.com/subgraphs/name/tohrnii/openlab-exchange-mumbai-c`
 
-    static query = `
-        query {
-            jobs(where: {status: 0}) {
-                id
-                jobURI
-            }
-        }
-    `
+  static query = `
+  query {
+      jobs(where: {status: 0}) {
+          id
+          jobURI
+      }
+  }
+  `
 
-    public async run(): Promise<void> {
-        const {
-            args,
-            flags,
-        } = await this.parse(ExchangeOpenJobs)
-        const result = await axios.post(
-            ExchangeOpenJobs.APIURL, 
-            {
-                query: ExchangeOpenJobs.query
-            }
-        )
-        const jobs = result.data.data.jobs
-        if (jobs.length == 0) {
-            this.log('NO OPEN JOBS')
-        } else {
-            this.log('OPEN JOBS...')
-            for (let i=0; i<jobs.length; i++) {
-                // Substring to remove '0x' prefix on job id
-                this.log(`Job ID: ${jobs[i].id.substring(2)}`)
-                // Substring to only provide CID (not with ipifs/)
-                this.log(`IPFS CID: ${jobs[i].jobURI.substring(5)}`)
-            }
-        }
+  public async run(): Promise<void> {
+    const result = await axios.post(
+      ExchangeOpenJobs.APIURL,
+      {
+        query: ExchangeOpenJobs.query
+      }
+    )
+    const jobs = result.data.data.jobs
+    if (jobs.length == 0) {
+      this.log('NO OPEN JOBS')
+    } else {
+      this.log('OPEN JOBS...')
+      for (let i = 0; i < jobs.length; i++) {
+        // Substring to remove '0x' prefix on job id
+        this.log(`Job ID: ${jobs[i].id.substring(2)}`)
+        // Substring to only provide CID (not with ipifs/)
+        this.log(`IPFS CID: ${jobs[i].jobURI.substring(5)}`)
+      }
     }
+  }
 }
