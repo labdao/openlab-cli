da0052c267b3c84451d9ba79d6137c5bbd4b47c2
Aakaash Meduri <aakaash.meduri@gmail.com>
2022-05-11 01:31:47 -0500

    config formatting

--- src/config.ts
+++ src/config.ts
@@ -12,20 +12,20 @@ export const defaults = {
     estuaryUploadUrl: 'https://shuttle-5.estuary.tech'
   },
   provider: {
-      maticMumbai: 'https://rpc-mumbai.matic.today',
-      alchemyMumbai: 'https://polygon-mumbai.g.alchemy.com/v2/RbQoqBm8Vp-taNd4jg7YRDB5t9QSoAAb'
+    maticMumbai: 'https://rpc-mumbai.matic.today',
+    alchemyMumbai: 'https://polygon-mumbai.g.alchemy.com/v2/RbQoqBm8Vp-taNd4jg7YRDB5t9QSoAAb'
   },
   contracts: {
-      maticMumbai: {
-          exchange: '0xfee53bffb6b70593478cd027cb2b52776fd8c064',
-          exchangeFactory: '0x53Eb5C8EF42D7261C0C2c9B8cF637a13B04f860A',
-          openLabNFT: '0x29bdc464C50F7680259242E5E2F68ab1FC75C964'
-      }
+    maticMumbai: {
+        exchange: '0xfee53bffb6b70593478cd027cb2b52776fd8c064',
+        exchangeFactory: '0x53Eb5C8EF42D7261C0C2c9B8cF637a13B04f860A',
+        openLabNFT: '0x29bdc464C50F7680259242E5E2F68ab1FC75C964'
+    }
   },
   tokens: {
-      maticMumbai: {
-          USD: '0x7fD2493c6ec0400be7247D6A251F00fdccc17375'
-      }
+    maticMumbai: {
+        USD: '0x7fD2493c6ec0400be7247D6A251F00fdccc17375'
+    }
   },
   subgraphs: {
     maticMumbai: {
