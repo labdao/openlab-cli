8d9def43e64b71f03d0651a69de35daefc4b2bf4
Rik Smith-Unna <rik@fathomlabs.io>
2022-05-13 13:12:52 +0100

    refactor account add to new ui

--- src/commands/account/add.ts
+++ src/commands/account/add.ts
@@ -1,4 +1,5 @@
 import { CliUx, Command } from '@oclif/core'
+import Listr from 'listr'
 import { checkMaticBalance, createWallet, drinkFromFaucet, importWallet } from '../../utils/wallet'
 
 export default class AccountAdd extends Command {
@@ -17,33 +18,64 @@ export default class AccountAdd extends Command {
     const importing = newWalletType === '2'
     const privateKey = importing && await CliUx.ux.prompt('Enter your private key')
 
-    const wallet = await (
-      importing ?
-      importWallet(password, privateKey) :
-      createWallet(password)
-    )
-    this.log(`Wallet created successfully!`)
-    this.log(`Your wallet address is ${wallet.address}`)
+    const list = new Listr([
+      {
+        title: 'Create wallet',
+        task: async (ctx, task) => {
+          const wallet = await (
+            importing ?
+              importWallet(password, privateKey) :
+              createWallet(password)
+          )
+          ctx.wallet = wallet
+          const op = importing ? 'imported' : 'created'
+          return `Wallet ${op}: ${wallet.address}`
+        }
+      },
+      {
+        title: 'Check wallet balance',
+        task: async (ctx, task) => {
+          const startbal = await checkMaticBalance(ctx.wallet.address)
+          ctx.startbal = startbal
+          return `Wallet balance: ${startbal} MATIC`
+        },
+      },
+      {
+        title: 'Get MATIC from faucet?',
+        skip: () => {
+          const getMatic = await CliUx.ux.confirm('Do you want to get some free MATIC tokens from the Polygon MATIC Faucet? (recommended)')
+          list.render()
+          if (!getMatic) {
+            return 'MATIC faucet skipped'
+          }
+        },
+        task: async (ctx, task) => {
+          task.title = `MATIC tokens requested from Mumbai testnet faucet...`
+          const res = await drinkFromFaucet(ctx.wallet.address)
+          if (res.hash === 'TRANSACTION_SENT_TO_DB') {
 
-    const getMatic = await CliUx.ux.confirm('Do you want to get some free MATIC tokens from the Polygon MATIC Faucet? (recommended)')
-    if (!getMatic) {
-      CliUx.ux.exit(1)
-    }
-    this.log(`Requesting a drop from the MATIC faucet`)
-    const res = await drinkFromFaucet(wallet.address)
-    if (res.hash === 'TRANSACTION_SENT_TO_DB') {
-      const startbal = await checkMaticBalance(wallet.address)
-      this.log('Starting balance:', startbal)
-      this.log(`MATIC tokens requested from Mumbai testnet faucet.`)
-      this.log('The MATIC may take a couple of minutes to show up in your account.')
-      this.log(`You can check your balance at https://mumbai.polygonscan.com/address/${wallet.address}`)
-    } else {
-      CliUx.ux.error('Failed to request tokens from faucet, please try again in a few minutes')
-    }
+          } else {
+            return 'Failed to request tokens from faucet, please try again in a few minutes'
+          }
+          return 'Drop requested. The MATIC may take a couple of minutes to show up in your account.'
+        }
+      },
+      {
+        title: '',
+        task: async (ctx, task) => {
+          task.
+            this.log(`MATIC tokens requested from Mumbai testnet faucet.`)
+          this.log('The MATIC may take a couple of minutes to show up in your account.')
+          this.log(`You can check your balance at https://mumbai.polygonscan.com/address/${ctx.wallet.address}`)
+        }
+      }
+    ],
+      {}
+    )
   }
 }
 
-async function getPassword(): Promise<string>  {
+async function getPassword(): Promise<string> {
   const password = await CliUx.ux.prompt(
     'Enter a password to encrypt the private key ' +
     '(You will not be able to recover the wallet if you forget this password)',
